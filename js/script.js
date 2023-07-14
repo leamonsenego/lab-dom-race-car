54bb81f093ed66a767dd3ffb36844e508d172b54
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-07-14 20:41:37 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +9,17 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+
+    game.start();
+  }
+
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
+
+  function restartGame() {
+    console.log("restart game");
+    location.reload();
   }
 };
