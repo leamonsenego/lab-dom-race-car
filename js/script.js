2beae584b08d76b2d341d5111602d6e1ef850739
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-07-15 10:06:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,43 @@ window.onload = function () {
     restartGame();
   });
 
+    // Function that handles keydown event
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
+};
+
+
   function restartGame() {
     console.log("restart game");
     location.reload();
